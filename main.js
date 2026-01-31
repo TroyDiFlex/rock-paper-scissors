1e47996662ddcf14643373c369523709880e8008
Artem Sokur <teebdol@bk.ru>
2026-01-31 22:06:04 +0400

    Подключил логику к UI (чистый коммит)

--- main.js
+++ main.js
@@ -1,64 +1,98 @@
+// Глобальные переменные
+const table = document.querySelector(".table");
+const popUpStart = document.querySelector(".start");
+
+const humanChoiceBtns = document.querySelector("#human-choice-btns");
+const humanChoiceValue = document.querySelector("human-choice");
+const computerChoiceValue = document.querySelector("#computer-choice");
+const runBtn = document.querySelector("#start");
+const resultValue = document.querySelector(".result");
+const resultValueDefault = "Выбирай чем будешь биться ты!<br /><p>пасхалка</p>";
+const popUpFinalResult = document.createElement("div");
+
+// Локальные переменные
 let humanScore = 0;
 let computerScore = 0;
+let humanChoice = null;
+let result = null;
+
+// Всплывающее окно финального результата с кнопкой рестарт
+popUpFinalResult.classList.add("pop-up", "result", "hide");
+popUpFinalResult.innerHTML = `
+<div class="popUpFinalResultBody"></div>
+<button id="btn-restart">Погнали</button>`;
+const popUpFinalResultBody = popUpFinalResult.querySelector(
+  ".popUpFinalResultBody",
+);
+table.append(popUpFinalResult);
+const restartBtn = popUpFinalResult.querySelector("#btn-restart");
+console.log(restartBtn);
+restartBtn.addEventListener("click", restart);
+
+// Логика игры
+runBtn.addEventListener("click", () => popUpStart.classList.toggle("hide"));
+
+humanChoiceBtns.addEventListener("click", (e) => {
+  if (e.target === humanChoiceBtns) return;
+  humanChoice = e.target.textContent.toLowerCase();
+  playRound(humanChoice);
+});
 
 const getComputerChoice = () => {
   let computerChoice = "";
   let randomNumber = Math.random();
 
   if (randomNumber < 1 / 3) {
-    computerChoice = "КАМЕНЬ";
+    computerChoice = "камень";
   } else if (randomNumber < 2 / 3) {
-    computerChoice = "НОЖНИЦЫ";
+    computerChoice = "ножницы";
   } else {
-    computerChoice = "БУМАГА";
+    computerChoice = "бумага";
   }
   return computerChoice;
 };
 
-const getHumanChoice = () => {
-  humanChoice = prompt();
-  return humanChoice.toUpperCase();
-};
-
-const playRound = () => {
-  let humanChoice = getHumanChoice();
+function playRound(humanChoice) {
   let computerChoice = getComputerChoice();
-  let win = `Ты выиграл! Компьютер выбрал ${computerChoice.toLowerCase()}`;
-  let lose = `Ты проиграл. Компьютер выбрал ${computerChoice.toLowerCase()}`;
-  let draw = `Ничья, компьютер тоже выбрал ${computerChoice.toLowerCase()}`;
+  let resultValueWin = `Ты выиграл раунд! Компьютер выбрал вариант "${computerChoice}"<br>Счёт ${humanScore}:${computerScore}`;
+  let resultValueLose = `Ты проиграл раунд. Компьютер выбрал вариант "${computerChoice}"<br>Счёт ${humanScore}:${computerScore}`;
+  let resultValueDraw = `Ничья, компьютер тоже выбрал вариант "${computerChoice}"<br>Счёт ${humanScore}:${computerScore}`;
 
-  if (humanChoice === computerChoice) {
-    console.log(draw);
+  if (humanChoice.toLowerCase() === computerChoice) {
+    resultValue.innerHTML = resultValueDraw;
   } else if (
-    (humanChoice === "КАМЕНЬ" && computerChoice === "НОЖНИЦЫ") ||
-    (humanChoice === "НОЖНИЦЫ" && computerChoice === "БУМАГА") ||
-    (humanChoice === "БУМАГА" && computerChoice === "КАМЕНЬ")
+    (humanChoice === "камень" && computerChoice === "ножницы") ||
+    (humanChoice === "ножницы" && computerChoice === "бумага") ||
+    (humanChoice === "бумага" && computerChoice === "камень")
   ) {
-    console.log(win);
+    resultValue.innerHTML = resultValueWin;
     humanScore++;
   } else {
-    console.log(lose);
+    resultValue.innerHTML = resultValueLose;
     computerScore++;
   }
-  console.log(`Счёт ${humanScore}:${computerScore}`);
-};
 
-const playGame = () => {
+  if (humanScore >= 3 || computerScore >= 3) {
+    pushFinalResutPopUp();
+  }
+}
+
+function restart() {
+  popUpFinalResult.classList.toggle("hide");
+}
+
+function pushFinalResutPopUp() {
+  if (humanScore > computerScore) {
+    result = "Поздравляю с победой!";
+  } else {
+    result = "Не повезло, бывает. Ты проиграл...";
+  }
+  popUpFinalResultBody.innerHTML = `<div class="popUpFinalResultBody">
+ <h1>${result}</h1>
+ <h3>Счёт ${humanScore}:${computerScore}</h3>
+ <h3>Сыграем ещё раз?</h3></div>`;
+  popUpFinalResult.classList.toggle("hide");
   humanScore = 0;
   computerScore = 0;
-
-  let scoreDraw = `Ничья...`;
-  let scoreWin = `Ты победил!`;
-  let scoreLose = `Ты проебал.`;
-
-  for (let i = 0; i < 5; i++) {
-    playRound();
-  }
-  if (humanScore === computerScore) {
-    console.log(scoreDraw);
-  } else if (humanScore > computerScore) {
-    console.log(scoreWin);
-  } else {
-    console.log(scoreLose);
-  }
-};
+  resultValue.innerHTML = resultValueDefault;
+}
